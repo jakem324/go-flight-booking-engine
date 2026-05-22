12bd2000a7831b67183f3e7d75c1a1debb5ebd8e
Jake Marshall <jake.marshall123@outlook.com>
2026-05-22 11:58:54 +1000

    Seed flight data

---
+++ postgres/migration/0000000008_seed_flights.up.sql
@@ -0,0 +1,5 @@
+insert into dbo.flight
+(id, flight_code, departure_date, departure_airport_code, departure_airport_terminal, arrival_date, arrival_airport_code, arrival_airport_terminal, max_available_seats)
+values
+('921640d1-603d-4078-9be5-1afdc5b6f780', 'JQ123', '2099-01-22 10:30:00', 'MEL', 'T4', '2099-01-22 12:30:00', 'SYD', 'T2', 250),
+('f7ca93e4-b4c0-43f6-92c0-7b7d941e853b', 'JQ456', '2099-03-25 06:00:00', 'MEL', 'T4', '2099-03-26 04:00:00', 'BCN', 'T1', 250);
