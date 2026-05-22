0ac6d91baa9f5cafc483c3264f8a3982fcc3cc6a
Jake Marshall <jake.marshall123@outlook.com>
2026-05-22 11:04:50 +1000

    Implement first endpoint

--- api/controllers.go
+++ api/controllers.go
@@ -2,14 +2,18 @@
 package api
 
 import (
-	//"context"
+	"context"
+	"errors"
 	"net/http"
+
+	"booking.engine/domain/commands"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func main() {
-	// ctx := context.Background()
-	// handlers := setup(ctx)
+	ctx := context.Background()
+	handlers := setup(ctx)
 
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
@@ -18,4 +22,36 @@ func main() {
       "message": "pong",
     })
   })
+
+	type CreatePencilBookingRequest struct {
+		RequiredNumberOfSeats int `json:"requiredNumberOfSeats" binding:"required"`
+		OutboundJourneyLegs []uuid.UUID `json:"outboundJourneyLegs" binding:"required"`
+	}
+
+	router.POST("/user", func(c *gin.Context) {
+		var json CreatePencilBookingRequest
+
+		// Bind incoming JSON to the struct
+		// ShouldBindJSON returns an error if the JSON is invalid or missing required fields
+		if jsonErr := c.ShouldBindJSON(&json); jsonErr != nil {
+				c.Status(http.StatusBadRequest)
+				return
+		}
+
+		createdBookingID, err := handlers.PencilBookingHandler.CreatePencilBooking(c, commands.CreatePencilBookingDto{
+			RequiredNumberOfSeats: json.RequiredNumberOfSeats,
+			OutboundJourneyLegs: json.OutboundJourneyLegs,
+		})
+
+		var seatsUnavailableError *commands.SeatsUnavailableError
+		if errors.As(err, &seatsUnavailableError) {
+			c.Status(http.StatusConflict)	
+			return
+		} else if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+
+		c.String(http.StatusOK, createdBookingID.String())
+	})
 }
